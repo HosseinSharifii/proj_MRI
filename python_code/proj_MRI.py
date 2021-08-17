f5d1ee3f8e7b9512422bb24447515ec48a504eea
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2021-08-17 00:03:46 -0400

    json instruct file

--- python_code/proj_MRI.py
+++ python_code/proj_MRI.py
@@ -10,3 +10,30 @@ import json
 import pandas as pd
 import numpy as np 
 
+
+def project_MRI_scanning(data_str = "",
+                            instruction_str="",
+                            projection_out_str=""):
+    """
+    Project a time-table for MRI scanning of mice.
+
+    Parameters
+    ----------
+    data_file_string : str, optional
+        Path to the data file. The default is "".
+    instruction_str : str, optional
+        Path to the .json instruction file. The default is "".
+    projection_out_str : str, optional
+        Path where the output projection is saved. The default is "".
+    
+
+    Returns
+    -------
+    proj_data : data spread sheet 
+        Data file containing the MRI scanning projection.
+  
+    """
+
+    
+if __name__=="__main__":
+    project_MRI_scanning()
